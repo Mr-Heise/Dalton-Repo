627a17d5959cb9d4277230a7667c4fecb4897062
ddavis12 <daltondavis@sasktel.net>
2017-06-07 10:58:19 -0600

    Add files via upload

---
+++ ParseString.java
@@ -0,0 +1,28 @@
+/*
+ * ParseString.java
+ * Dalton Davis
+ * CSC30
+ * Start Date: Wednesday 7 June 2017
+ * This program simulates processing a line of text from a text file.
+ */
+ 
+/**
+ * The ParseString class simulates processing a line of text from a text file.
+ */
+ 
+public class ParseString
+{
+	
+	public static void main(String[] args)
+	{
+	
+		String lineOfText = "Babyak,Akilah,2000,6,June";
+		
+		System.out.println("\n\t*** THE PARSE STRING SIMULATION PROGRAM***\n");
+			
+			
+			
+	}
+		
+		
+}
