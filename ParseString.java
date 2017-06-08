c753817dd15d5281dbd8aefb350c8c29c3693a35
Mr-Heise <mr.heise@gmail.com>
2017-06-08 10:41:47 -0600

    Add files via upload

--- ParseString.java
+++ ParseString.java
@@ -1,28 +1,96 @@
 /*
  * ParseString.java
- * Dalton Davis
+ * Mr. Heise
  * CSC30
  * Start Date: Wednesday 7 June 2017
  * This program simulates processing a line of text from a text file.
  */
- 
+
+
+
+
 /**
  * The ParseString class simulates processing a line of text from a text file.
  */
- 
-public class ParseString
-{
-	
-	public static void main(String[] args)
-	{
+
+
+public class ParseString {
+
+	public static void main(String[] args){
 	
 		String lineOfText = "Babyak,Akilah,2000,6,June";
 		
-		System.out.println("\n\t*** THE PARSE STRING SIMULATION PROGRAM***\n");
-			
-			
-			
+		String lastName;
+		String firstName;
+		
+		
+		int birthYear;
+		int birthMonth;
+		
+		int comma;
+		
+		
+		
+		
+		System.out.println("\n\t*** THE PARSE STRING SIMULATION PROGRAM ***\n");
+		
+		
+		comma = lineOfText.indexOf(",");
+		
+		lastName = lineOfText.substring(0,comma);
+		
+		System.out.println("\n\tlastName = " + lastName);
+		
+		
+		
+		lineOfText = lineOfText.substring(comma + 1);
+		
+		//System.out.println("\n\tlineOfText = " + lineOfText);
+		
+		
+		
+		comma = lineOfText.indexOf(",");
+		
+		firstName = lineOfText.substring(0,comma);
+		
+		System.out.println("\n\tfirstName = " + firstName);
+		
+		
+		
+		lineOfText = lineOfText.substring(comma + 1);
+		
+		//System.out.println("\n\tlineOfText = " + lineOfText);
+		
+		
+		
+		comma = lineOfText.indexOf(",");
+		
+		birthYear = Integer.parseInt(lineOfText.substring(0,comma));
+		
+		System.out.println("\n\tbirthYear = " + birthYear);
+		
+		
+	
+		lineOfText = lineOfText.substring(comma + 1);
+	
+		//System.out.println("\n\tlineOfText = " + lineOfText);
+		
+		
+		comma = lineOfText.indexOf(",");
+		
+		birthMonth = Integer.parseInt(lineOfText.substring(0,comma));
+		
+		System.out.println("\n\tbirthMonth = " + birthMonth);
+		
+		
+		
+		
+		System.out.println();
+		
+	
+	
+	
 	}
-		
-		
+
+
 }
